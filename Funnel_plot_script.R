06e6dd0d40aa8afab8944372293f4ca4427378ad
Chris <chrismainey@gmail.com>
2019-09-04 22:38:02 +0100

    Updated content prior to RSS

---
+++ Funnel_plot_script.R
@@ -0,0 +1,33 @@
+library(FunnelPlotR)
+library(COUNT)
+library(ggplot2)
+
+# lets use the \'medpar\' dataset from the \'COUNT\' package. Little reformatting needed
+data(medpar)
+write.csv(medpar, "medpar.csv")
+rm(medpar)
+medpar <- read.csv(medpar)
+medpar$provnum<-factor(medpar$provnum)
+medpar$los<-as.numeric(medpar$los)
+
+mod<- glm(died ~ los + hmo + age80 + factor(type), family="poisson", data=medpar)
+
+medpar$prds<- predict(mod, type="response")
+
+a<-funnel_plot(numerator=medpar$died, denominator=medpar$prds, group = medpar$provnum,
+            title = 'Mortality Ratio plot for `medpar` data', Poisson_limits = TRUE,
+            OD_adjust = FALSE,label_outliers = TRUE, return_elements = "plot")
+
+a[[1]] + theme(legend.position = "bottom")
+
+
+mod2<- glm(los ~ hmo + died + age80 + factor(type), family="poisson", data=medpar)
+summary(mod2)
+
+medpar$prds2<- predict(mod2, type="response")
+
+b<-funnel_plot(numerator=medpar$los, denominator=medpar$prds2, group = medpar$provnum,
+            title = 'Length of Stay Funnel plot for `medpar` data', Poisson_limits = FALSE,
+            OD_adjust = TRUE, label_outliers = TRUE, return_elements = "plot")
+
+b[[1]] + theme(legend.position = "bottom")
--- README.md
+++ README.md
@@ -6,13 +6,11 @@ Chris Mainey
 
 ## R Markdown
 
-This repository contains the slides, information and resources used form
-my talk at the Royal Statistical Society Conference 2019, on 5th
-September 2019.
+This repository contains the slides, a couple of scripts used for my
+talk at the Royal Statistical Society Conference 2019, on 5th September
+2019.
 
-It is a work in progress, as it’s a long way off.
-
-All section will be written in R, with slides using using `xaringan` see
-[git repo](https://github.com/yihui/xaringan), based on the `medpar`
-data set in the `COUNT` package see
+I did want to share more, but I can’t share patient data, nor anything
+that is UH intellectual properly. Instead, we will use the Medicare
+dataset `medpar` in the `COUNT` package see
 [CRAN](https://cran.r-project.org/web/packages/COUNT/index.html) entry.
